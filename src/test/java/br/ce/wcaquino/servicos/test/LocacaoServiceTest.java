ed082981121151bca47a421b5d207c5832375ea3
Pasconsa <saulopascon@hotmail.com>
2023-02-02 11:37:30 -0300

    2.06 Forma dividir dividir testes

--- src/test/java/br/ce/wcaquino/servicos/test/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/test/LocacaoServiceTest.java
@@ -8,7 +8,9 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matcher;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -17,6 +19,9 @@ import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
 	
 	@Test
 	public void test() {
@@ -29,10 +34,9 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		//verificacao
-		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(5.0)));
-		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.0)));
-		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
-		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+		error.checkThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(6.0)));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(false));
 	}
 
 	
